b846dabdfc75305fc6b97875cd060080e10c20fb
Suvradeep Chakraborty <66739102+Suvra007@users.noreply.github.com>
2020-06-17 23:09:42 +0530

    Add files via upload

---
+++ NONLINEAR CLASSIFICATION OF FLOWER DATA USING SVM KERNEL.py
@@ -0,0 +1,33 @@
+from sklearn import datasets
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+iris=datasets.load_iris()
+X=iris.data[:,[2,3]]
+
+X1=iris.data[50:150,[2,3]]
+y=iris.target[50:150]
+X1s=X1.copy()
+X1s=(X1-X1.mean())/X1.std()
+from sklearn.svm import SVC
+svm=SVC(kernel="rbf",C=1.0,random_state=0)
+svm.fit(X1s,y)
+def pdr(X,y,classifier,resolution=0.02):
+    markers=("s","x","o","^","v")
+    colors=("red","blue","lightgreen","gray","cyan")
+    cmap=ListedColormap(colors[:len(np.unique(y))])
+    x1_min,x1_max=X[:,0].min()-1,X[:,0].max()+1
+    x2_min,x2_max=X[:,1].min()-1,X[:,1].max()+1
+    xx1,xx2=np.meshgrid(np.arange(x1_min,x1_max,resolution),np.arange(x2_min,x2_max,resolution))
+    Z=classifier.predict(np.array([xx1.ravel(),xx2.ravel()]).T)
+    Z=Z.reshape(xx1.shape)
+    plt.contourf(xx1,xx2,Z,alpha=0.4,cmap=cmap)
+    plt.xlim(xx1.min(),xx1.max())
+    plt.ylim(xx2.min(),xx2.max())
+    for idx,cl in enumerate(np.unique(y)):
+        plt.scatter(x=X[y==cl,0],y=X[y==cl,1],alpha=0.8,c=cmap(idx),marker=markers[idx],label=cl)
+                    
+pdr(X1s,y,classifier=svm)
+plt.legend()
+plt.show()
+    
